4f6e01b9a45c3849e8e583e765a468d169dc5dde
Futaba-Kosuke <k.futaba2828@gmail.com>
2021-07-19 19:25:19 +0900

    Fix: 複数回の区間登録に対応 (#10)

--- firebase/main.py
+++ firebase/main.py
@@ -1,4 +1,4 @@
-from typing import Final, TypedDict
+from typing import Final, Union, TypedDict
 
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -26,11 +26,11 @@ class Firebase:
             'user_id': user_id,
             'commuter_pass': commuter_pass
         }
-        self.db.collection(COLLECTION_NAME).add(user_preference)
+        self.db.collection(COLLECTION_NAME).document(user_id).set(user_preference)
 
-    def get_user_commuter_pass(self, user_id: str) -> CommuterPassType:
-        response = self.db.collection(COLLECTION_NAME).where('user_id', '==', user_id).stream()
-        for user_preference in response:
-            commuter_pass: CommuterPassType = user_preference.to_dict()['commuter_pass']
-            break
-        return commuter_pass
+    def get_user_commuter_pass(self, user_id: str) -> Union[CommuterPassType, str]:
+        response = self.db.collection(COLLECTION_NAME).document(user_id).get()
+        if response.exists:
+            return response.to_dict()['commuter_pass']
+        else:
+            return 'Not Found'
